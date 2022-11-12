c6c7584c80404d90ddcde411b92330088a3c8e92
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-12 16:46:03 +0600

    services section added

---
+++ src/Pages/Home/Services/ServiceCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ServiceCard = ({ service }) => {
+  const { title, description, icon } = service;
+  return (
+    <div className="p-8 rounded shadow-md text-center flex flex-col items-center mx-auto">
+      <img src={icon} alt={title} className="w-24" />
+      <h2 className="font-semibold text-xl mt-8">{title}</h2>
+      <p className="font-normal text-base mt-4">{description}</p>
+    </div>
+  );
+};
+
+export default ServiceCard;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,7 +1,48 @@
 import React from "react";
+import fluoride from "../../../assets/images/fluoride.png";
+import cavity from "../../../assets/images/cavity.png";
+import whitening from "../../../assets/images/whitening.png";
+import ServiceCard from "./ServiceCard";
 
 const Services = () => {
-  return <div></div>;
+  const services = [
+    {
+      id: 1,
+      title: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      icon: fluoride,
+    },
+    {
+      id: 2,
+      title: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      icon: cavity,
+    },
+    {
+      id: 3,
+      title: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      icon: whitening,
+    },
+  ];
+  return (
+    <div className="my-40">
+      <div className="text-center my-20">
+        <h2 className="uppercase font-bold text-2xl text-secondary">
+          our services
+        </h2>
+        <p className="text-3xl font-normal mt-1">Services We Provide</p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 ">
+        {services.map(service => (
+          <ServiceCard key={service.id} service={service}></ServiceCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
