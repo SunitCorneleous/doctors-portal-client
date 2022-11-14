35bb613c8f98c728c6a7b56ccde8bb755bf1f226
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-15 00:13:30 +0600

    react form hook added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,23 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const handleLogin = data => {
+    console.log(data);
+  };
+
   return (
     <div className="h-[80vh] flex justify-center items-center">
       <div className="w-11/12 md:w-[370px] md:h-[480px] p-5 shadow-lg rounded-lg">
         <h3 className="text-center text-xl font-normal">Login</h3>
-        <form>
+        <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Email</span>
             </label>
-            <input type="email" className="input input-bordered w-full" />
+            <input
+              {...register("email")}
+              type="email"
+              className="input input-bordered w-full"
+            />
           </div>
           <div className="form-control w-full">
             <label className="label">
               <span className="label-text">Password</span>
             </label>
-            <input type="password" className="input input-bordered w-full" />
+            <input
+              {...register("password")}
+              type="password"
+              className="input input-bordered w-full"
+            />
             <label className="label">
               <span className="label-text-alt">
                 <Link className="hover:underline">Forgot password?</Link>
